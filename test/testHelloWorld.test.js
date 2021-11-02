e642ccfa74328b279e0e9ffa15ffc79c2f42c2bc
Kasper de Graaff <kasperdegraaff1997@gmail.com>
2021-11-02 13:39:31 +0100

    first commit

---
+++ test/testHelloWorld.test.js
@@ -0,0 +1,13 @@
+const HelloWorld = artifacts.require("HelloWorld")
+
+contract("HelloWorld", () => {
+  let contract
+})
+
+contract("HelloWorld test", async accounts => {
+  it("string helloWorld should be Hello World!", async () => {
+    const instance = await HelloWorld.deployed();
+    const string = await instance.helloWorld.call()
+    assert.equal(string, "Hello World!");
+  })
+})
